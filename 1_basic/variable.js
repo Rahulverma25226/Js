ac98c7fdd77aa836ab486314813512b58a3fd455
Rahulverma25226 <164183410+Rahulverma25226@users.noreply.github.com>
2025-02-06 00:09:05 +0530

    variable commit

---
+++ 1_basic/variable.js
@@ -0,0 +1,14 @@
+const accountId = '12345';
+let accountEmail = "rahul2121@gmail.com"; 
+var accountPassword = "12345";
+accountCity = "Sirsa";
+
+accountEmail = "verma223@gmail.com";
+accountPassword = "123456";
+accountCity = "Delhi";
+
+// console.log(accountId);
+
+// prefer not to use var beacuse it is function scope
+
+console.table([accountId, accountEmail, accountPassword, accountCity]);
