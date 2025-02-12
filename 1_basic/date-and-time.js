971e65e1cac596ae54c2b721bd7d228e4e200dbc
Rahulverma25226 <164183410+Rahulverma25226@users.noreply.github.com>
2025-02-12 15:40:34 +0530

    date-time

---
+++ 1_basic/date-and-time.js
@@ -0,0 +1,14 @@
+// Date
+// Date is object in js
+
+// let myDate = new Date()
+console.log(myDate)
+// console.log(myDate.toString())
+// console.log(myDate.toLocaleString())
+// console.log(typeof myDate) // Object
+
+// let myDate = new Date(2023, 0, 23)
+// console.log(myDate.toDateString());
+
+let myDate = new Date("2023, 0, 23, 5, 3")
+console.log(myDate.toLocaleString());
