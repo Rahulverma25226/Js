e9007a7a6174fdc1a23530bed299bef1cbd8c997
Rahulverma25226 <164183410+Rahulverma25226@users.noreply.github.com>
2025-02-12 16:19:16 +0530

    Last-commit

--- 1_basic/date-and-time.js
+++ 1_basic/date-and-time.js
@@ -2,13 +2,23 @@
 // Date is object in js
 
 // let myDate = new Date()
-console.log(myDate)
+// console.log(myDate)
 // console.log(myDate.toString())
 // console.log(myDate.toLocaleString())
-// console.log(typeof myDate) // Object
+// console.log(typeof myDate) 
 
 // let myDate = new Date(2023, 0, 23)
-// console.log(myDate.toDateString());
+// let myDate = new Date(2023, 0, 23, 5, 3)
+// let myDateName = new Date("2023-01-14")
+// let myDateName = new Date("01-14-2023")
+// console.log(myDateName.toLocaleString());
 
-let myDate = new Date("2023, 0, 23, 5, 3")
-console.log(myDate.toLocaleString());
+// let myTimeStamp = Date.now()
+// console.log(myTimeStamp); // milliseconds
+// console.log(myDateName.getTime()) // second
+// console.log(Math.floor(Date.now()/1000)) // second
+
+let newDate = new Date()
+console.log(newDate.getFullYear());
+console.log(newDate.getMonth() + 1);
+console.log(newDate.getDay());
